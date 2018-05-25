68337c5c9bd5f2359569514ac489d72cf77f256f
Moker_C <15216656514@163.com>
2018-05-25 16:54:49 +0800

    Initial Commit

---
+++ 水印图片/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  水印图片
+//
+//  Created by Mac1 on 2018/5/25.
+//  Copyright © 2018年 Mac1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 水印图片/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  水印图片
+//
+//  Created by Mac1 on 2018/5/25.
+//  Copyright © 2018年 Mac1. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
